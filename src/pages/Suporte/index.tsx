5f3d482bf39fb1f0c71b409d9967204fa893e4fe
rogerxavier29 <rogerdpx@gmail.com>
2022-04-07 18:04:36 -0300

    feito a parte inicial da parte de suporte

--- src/pages/Suporte/index.tsx
+++ src/pages/Suporte/index.tsx
@@ -1,8 +1,92 @@
-import React from 'react';
-import { Content } from './styles';
+import React from "react";
+import { Content, Form } from "./styles";
+import { useFormik } from "formik";
+import { FormControl, TextField, Grid } from "@material-ui/core";
 
 const Suporte: React.FC = () => {
-  return <Content>Suporte</Content>;
+  // const formik = useFormik({
+  //   initialValues: {},
+  // });
+
+  return (
+    <Content id="suporte">
+      <Form>
+        <h1>SUPORTE</h1>
+        <form>
+          <Grid container spacing={4}>
+            <Grid xs={12} md={4}>
+              <FormControl>
+                <TextField
+                  id="name"
+                  name="name"
+                  label="Nome"
+                  variant="outlined"
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid xs={12} md={4}>
+              <FormControl>
+                <TextField
+                  id="sobrenome"
+                  name="sobrenome"
+                  label="Sobrenome"
+                  variant="outlined"
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={2} columns={16}>
+            <Grid xs={12} md={4}>
+              <FormControl>
+                <TextField
+                  id="email"
+                  name="email"
+                  label="E-mail"
+                  variant="outlined"
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid xs={12} md={4}>
+              <FormControl>
+                <TextField
+                  id="apelido"
+                  name="apelido"
+                  label="Apelido de jogador"
+                  variant="outlined"
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={4}>
+            <Grid xs={16} md={4}>
+              <FormControl>
+                <TextField
+                  id="email"
+                  name="email"
+                  label="E-mail"
+                  variant="outlined"
+                />
+              </FormControl>
+            </Grid>
+            <Grid xs={16} md={4}>
+              <FormControl>
+                <TextField
+                  id="email"
+                  name="email"
+                  label="E-mail"
+                  variant="outlined"
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+        </form>
+      </Form>
+    </Content>
+  );
 };
 
 export default Suporte;
--- src/pages/Suporte/styles.js
+++ src/pages/Suporte/styles.js
@@ -1,5 +1,27 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Content = styled.div`
-  background: silver;
+  margin-top: 80px;
+  display: grid;
+  justify-content: flex-start;
+  margin-left: 50px;
+`;
+
+export const Form = styled.div`
+  display: grid;
+  width: 900px;
+  height: auto;
+  background: #fff;
+  border-radius: 5px;
+  opacity: 1;
+  margin: 30px;
+  padding: 30px;
+  form {
+    display: grid;
+    gap: 50px;
+  }
+  h1 {
+    display: flex;
+    justify-content: start;
+  }
 `;
