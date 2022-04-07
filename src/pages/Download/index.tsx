ea6c5a35daf07ffb8012936e1ea4153f027cc696
rogerxavier29 <rogerdpx@gmail.com>
2022-04-07 13:51:47 -0300

    feito a parte de download

--- src/pages/Download/index.tsx
+++ src/pages/Download/index.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
-import { Content } from './styles';
+import React from "react";
+import { Content, Image, Info, Button } from "./styles";
+import IconAmigos from "../../assets/icon-amigos.png";
+import IconCarrinho from "../../assets/icon-carrinho.png";
+import IconTaca from "../../assets/icon-taca.png";
+import ImgDownload from "../../assets/img-download.png";
+import IconPC from "../../assets/icon-pc.png";
 
 const Download: React.FC = () => {
-  return <Content>Download</Content>;
+  return (
+    <Content id="download">
+      <Info>
+        <h1>BAIXE O JOGO</h1>
+        <li>
+          {" "}
+          <img src={IconAmigos} alt="" className="icons" />{" "}
+          <p>Jogue com os seus amigos</p>
+        </li>
+        <li>
+          <img src={IconCarrinho} alt="" className="icons" />{" "}
+          <p>Compre itens digitais</p>
+        </li>
+        <li>
+          <img src={IconTaca} alt="" className="icons" />{" "}
+          <p>Torneios independentes</p>
+        </li>
+        <Button>
+          {" "}
+          <img src={IconPC} alt="" /> <p>DOWNLOAD</p>
+        </Button>
+      </Info>
+      <Image>
+        <img src={ImgDownload} alt="" />
+      </Image>
+    </Content>
+  );
 };
 
 export default Download;
--- src/pages/Download/styles.js
+++ src/pages/Download/styles.js
@@ -1,5 +1,59 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Content = styled.div`
-  background: silver;
+  margin-top: 80px;
+  display: flex;
+  justify-content: space-between;
+  width: 900px;
+`;
+
+export const Info = styled.div`
+  display: grid;
+  color: #fff;
+  margin-left: 50px;
+  li {
+    display: flex;
+    margin-left: 40px;
+    gap: 20px;
+    p {
+      font: normal normal 300 17px Poppins;
+    }
+  }
+  img.icons {
+    width: 34px;
+    height: 25px;
+    margin-top: 10px;
+  }
+  h1 {
+    font-style: italic;
+    font-size: 25px;
+    margin-bottom: 60px;
+  }
+`;
+
+export const Image = styled.div`
+  margin-top: 60px;
+  img {
+    width: 350px;
+    height: 350px;
+  }
+`;
+
+export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  width: 183px;
+  height: 36px;
+  border: 1px solid #e13157;
+  background: #e13157;
+  border-radius: 5px;
+  margin-left: 50px;
+  color: #fff;
+  cursor: pointer;
+  img {
+    margin-top: 8px;
+  }
+  p {
+    margin-left: 10px;
+  }
 `;
